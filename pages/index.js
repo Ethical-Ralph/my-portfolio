e194158598cc672625b2cea89f22f30afaa76d33
Akinola Raphael <54055273+Ethical-Ralph@users.noreply.github.com>
2021-08-16 14:07:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <main>
         <h1 className="title">Akinola Raphael</h1>
 
-        <p className="description">welcome to my portfolio</p>
+        
       </main>
 
       <style jsx>{`
